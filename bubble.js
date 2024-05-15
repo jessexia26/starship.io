b640ec2db27ae7a7941f5d83c7c6569f677277a6
yang <caoyang1128@foxmail.com>
2024-05-14 20:49:56 -0700

    Add

---
+++ bubble.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('goHome').addEventListener('click', function() {
+        window.location.href = 'https://jessexia-0526.wixstudio.io/jesse/studio';
+    });
+});
